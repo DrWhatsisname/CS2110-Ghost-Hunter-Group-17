603878112745a0cb5fbbcd8ad40d888d7c9ed687
Wyliew <wylie.w.wang@gmail.com>
2014-11-07 09:04:03 -0500

    Fixed Splash Screen

---
+++ GhostHunter17/src/edu/virginia/cs2110/ghosthunter17/SplashScreen.java
@@ -0,0 +1,78 @@
+package edu.virginia.cs2110.ghosthunter17;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.Window;
+
+public class SplashScreen extends Activity {
+	
+	 private Thread mSplashThread; 
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_splash_screen);
+		final SplashScreen sPlashScreen = this;   
+        // The thread to wait for splash screen events
+        mSplashThread =  new Thread(){
+            @Override
+            public void run(){
+                try {
+                    synchronized(this){
+                        // Wait given period of time or exit on touch
+                        wait(10000);
+                    }
+                }
+                catch(InterruptedException ex){                    
+                }
+
+                finish();
+                
+                // Run next activity
+                Intent intent = new Intent();
+                intent.setClass(sPlashScreen, MainActivity.class);
+                startActivity(intent);
+                //stop();                             
+            }
+        };
+        
+        mSplashThread.start(); 
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.splash_screen, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	
+	@Override
+    public boolean onTouchEvent(MotionEvent evt)
+    {
+        if(evt.getAction() == MotionEvent.ACTION_DOWN)
+        {
+            synchronized(mSplashThread){
+                mSplashThread.notifyAll();
+            }
+        }
+        return true;
+    }   
+}
